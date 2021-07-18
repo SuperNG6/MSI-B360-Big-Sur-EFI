d902d0c8923408e51ff17e5c542b2a93ee260418
SuperNG6 <sleele@gmail.com>
2021-07-18 10:01:14 +0800

    update workflows

---
+++ .github/workflows/update-changelog.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# get release tag
+NVMeFix_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'NVMeFix' | cut -d= -f2-)
+IntelMausi_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'IntelMausi' | cut -d= -f2-)
+VirtualSMC_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'VirtualSMC' | cut -d= -f2-)
+Lilu_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'Lilu' | cut -d= -f2-)
+WhateverGreen_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'WhateverGreen' | cut -d= -f2-)
+OpenCorePkg_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'OpenCorePkg' | cut -d= -f2-)
+AppleALC_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'AppleALC' | cut -d= -f2-)
+
+# generate Changelog.md
+cat > Changelog.md << EOF
+1、update config.plist & support macOS Big Sur 11.4
+2、update OpenCorePkg & Kexts to the latest
+
+| Components    | Version               |
+| ------------- | --------------------- |
+| OpenCorePkg   | ${OpenCorePkg_TAG}    | 
+| AppleALC      | ${AppleALC_TAG}       |
+| IntelMausi    | ${IntelMausi_TAG}     |
+| Lilu          | ${Lilu_TAG}           |
+| VirtualSMC    | ${VirtualSMC_TAG}     |
+| WhateverGreen | ${WhateverGreen_TAG}  |
+| NVMeFix       | ${NVMeFix_TAG}        |
+
+EOF
