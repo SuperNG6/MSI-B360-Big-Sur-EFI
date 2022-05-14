0d5dc8e113feeb3440ee87bf9b5558f2fa1119a5
SuperNG6 <sleele@gmail.com>
2022-05-14 14:22:49 +0800

    added support for AMD RX550Lexa+IGPU platform

--- .github/workflows/update-changelog.sh
+++ .github/workflows/update-changelog.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # get release tag
-NVMeFix_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'NVMeFix' | cut -d= -f2-)
-IntelMausi_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'IntelMausi' | cut -d= -f2-)
-VirtualSMC_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'VirtualSMC' | cut -d= -f2-)
-Lilu_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'Lilu' | cut -d= -f2-)
-WhateverGreen_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'WhateverGreen' | cut -d= -f2-)
-OpenCorePkg_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'OpenCorePkg' | cut -d= -f2-)
-AppleALC_TAG=$(wget -qO- https://git.io/Acidanthera-tags | grep 'AppleALC' | cut -d= -f2-)
+NVMeFix_TAG=$(wget -qO- https://raw.githubusercontent.com/SuperNG6/Acidanthera-Hackintosh-Tools/main/ReleaseTag | grep 'NVMeFix' | cut -d= -f2-)
+IntelMausi_TAG=$(wget -qO- https://raw.githubusercontent.com/SuperNG6/Acidanthera-Hackintosh-Tools/main/ReleaseTag | grep 'IntelMausi' | cut -d= -f2-)
+VirtualSMC_TAG=$(wget -qO- https://raw.githubusercontent.com/SuperNG6/Acidanthera-Hackintosh-Tools/main/ReleaseTag | grep 'VirtualSMC' | cut -d= -f2-)
+Lilu_TAG=$(wget -qO- https://raw.githubusercontent.com/SuperNG6/Acidanthera-Hackintosh-Tools/main/ReleaseTag | grep 'Lilu' | cut -d= -f2-)
+WhateverGreen_TAG=$(wget -qO- https://raw.githubusercontent.com/SuperNG6/Acidanthera-Hackintosh-Tools/main/ReleaseTag | grep 'WhateverGreen' | cut -d= -f2-)
+OpenCorePkg_TAG=$(wget -qO- https://raw.githubusercontent.com/SuperNG6/Acidanthera-Hackintosh-Tools/main/ReleaseTag | grep 'OpenCorePkg' | cut -d= -f2-)
+AppleALC_TAG=$(wget -qO- https://raw.githubusercontent.com/SuperNG6/Acidanthera-Hackintosh-Tools/main/ReleaseTag | grep 'AppleALC' | cut -d= -f2-)
 
 # generate Changelog.md
 cat > Changelog.md << EOF
 1、update config.plist
 2、update OpenCorePkg & Kexts to the latest
-3、support macOS Monterey 12.3
+3、support macOS Monterey 12.3.1
+4、added support for AMD RX550Lexa+IGPU platform
 
 | Components    | Version               |
 | ------------- | --------------------- |
